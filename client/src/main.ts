76b95e870d1e0ac01d6500dce0d790714af63b22
peter9811 <petercuevas.6@gmail.com>
2023-05-26 07:26:00 -0500

    Update main.ts

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,33 +1,25 @@
 import { createApp } from 'vue';
-import { Quasar, Dialog, Notify } from 'quasar';
+import { Dialog, Notify } from 'quasar';
 import { get1t } from './scripts/onetagger';
 import router from './scripts/router';
 import iconSet from 'quasar/icon-set/mdi-v6';
 
-// Style
 import '@quasar/extras/mdi-v6/mdi-v6.css';
 import 'quasar/src/css/index.sass';
 import './style/app.scss';
 
 import App from './App.vue';
 
-
-// Handle Windows webview messages
-// @ts-ignore
 if (window.chrome && window.chrome.webview) {
-    // @ts-ignore
-    window.chrome.webview.addEventListener('message', e => {
+    window.chrome.webview.addEventListener('message', (e: MessageEvent) => {
         get1t().onOSMessage(JSON.parse(e.data));
     });
 }
 
-
 createApp(App)
     .use(router)
     .use(Quasar, {
-        plugins: {
-            Dialog, Notify
-        },
+        plugins: [Dialog, Notify],
         iconSet
     })
     .mount('#app');
